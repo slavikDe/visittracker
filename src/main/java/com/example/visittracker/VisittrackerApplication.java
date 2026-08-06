d694c3a513672004a5b488bf7bbcecf7b31e2fc2
Viacheslav Denysenko <denysenkoo.viacheslav@gmail.com>
2026-08-06 16:36:51 +0300

    init

---
+++ src/main/java/com/example/visittracker/VisittrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.visittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisittrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisittrackerApplication.class, args);
+	}
+
+}
